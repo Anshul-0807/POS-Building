2d0e19626f18b2ba701b9a1ea3cc1d2b3526d9bd
Anshul patle <patleanshul920@gmail.com>
2023-03-05 17:10:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline text-sky-200">
-      welcome to pos
+    <h1 className="text-3xl font-bold underline text-sky-300">
+      welcome to pos!
     </h1>
   )
 }
