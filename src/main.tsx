01cbe9766cecf96ca8f5d47260f6a2da7fe28942
Anshul patle <patleanshul920@gmail.com>
2023-03-10 15:27:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import App from './App'
 import './index.css'
+import About from './pages/About';
+import MainLayout from './layouts/MainLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-   <div className='bg-gray-900'>
-   <App />
-   </div>
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
